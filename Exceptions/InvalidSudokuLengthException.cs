a4683738eb2fdb23ca4e4229ec0d50b48ef7e8cb
ori asulin <oriasulin0909@gmail.com>
2025-01-18 18:14:09 +0200

    feat: add initial validator

---
+++ Exceptions/InvalidSudokuLengthException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Exceptions
+{
+    internal class InvalidSudokuLengthException : Exception
+    {
+
+        public InvalidSudokuLengthException(string message)
+            :base(message){ }
+    }
+}
---
+++ InputHandling/InputValidation.cs
@@ -0,0 +1,33 @@
+﻿using Sudoku.Exceptions;
+
+namespace Sudoku.InputHandling
+{
+    public class InputValidation
+    {
+        /* 
+        This class validates the given sudoku input.
+        Note that this class dosen't determine if a suduko
+        is solvable or not.
+         */
+        public static void ValidateInput(string rawInput, int dimensions)
+        {
+            //Invalid characters found in the input
+            string invalidCharacters = "";
+            // Validate input length 
+            if (rawInput.Length != dimensions * dimensions)
+                throw new InvalidSudokuLengthException($"Invalid input length for given dimensions ({dimensions} X {dimensions})");
+
+            foreach (char token in rawInput)
+            {
+                if (!char.IsDigit(token))
+                {
+                    invalidCharacters += token;   
+                }
+            }
+            // Found Illegal tokens
+            if (invalidCharacters.Length != 0)
+                throw new IllegalCharactersException($"found illegal chracters in input : {invalidCharacters}");
+            
+        }
+    }
+}
