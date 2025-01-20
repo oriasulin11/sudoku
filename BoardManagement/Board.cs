611aa7bb1959a92d6b7b2345bd7dff5065c479ad
ori asulin <oriasulin0909@gmail.com>
2025-01-20 21:04:14 +0200

    feat: add board implementation

---
+++ BoardManagement/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Sudoku.Board  
+{
+    /// <summary>
+    /// The sudoku board will be represented as a matrix
+    /// of hash sets. Each set will include the numbers
+    /// which can fit in the cell.
+    /// The board size may vary according to the _dimensions field.
+    /// </summary>
+    internal class Board
+    {
+        
+        private readonly int _dimensions;
+        private readonly ISet<int>[,] _board;
+
+        public Board(int dimensions)
+        {
+            _dimensions= dimensions;
+            _board = new HashSet<int>[dimensions, dimensions];
+
+            //initiating each cell
+            for (int row = 0; row < dimensions; row++)
+            {
+                for (int column = 0; column < dimensions; column++)
+                {
+                    _board[row,column] = new HashSet<int>();
+                }
+            }
+
+        }
+    }
+}
