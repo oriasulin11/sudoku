985c66ae53bb2b922009fa98ec018aeeaacd6691
ori asulin <oriasulin0909@gmail.com>
2025-01-24 20:37:46 +0200

    feat: add neighbor updater

---
+++ SolvingUnit/NeighborsUpdater.cs
@@ -0,0 +1,78 @@
+﻿using Sudoku.BoardManagement;
+using System;
+using System.Collections.Generic;
+
+namespace Sudoku.SolvingUnit
+{
+    /// <summary>
+    /// This class updates the possible values of
+    /// cells in the same row, column and box.
+    /// </summary>
+    internal static class NeighborsUpdater
+    {
+        public static void UpdateNeighbors(Board board, int row, int column, int value)
+        {
+            
+        }
+        /// <summary>
+        /// Iterate over the row and remove the number given from
+        /// the possible values in the cell 
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <param name="value"></param>
+        public static void UpdateRow(Board board, int row, int column, int value)
+        {
+            for (int index = 0; index < board.Dimensions; index++)
+            {
+                Cell cell = board.GetCell(row, index);
+                if(cell.FinalValue == 0 && cell.PossibleValues.Contains(value))
+                    cell.PossibleValues.Remove(value);
+
+            }
+        }
+        /// <summary>
+        ///  Iterate over the column and remove the number given from
+        ///  the possible values in the cell
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <param name="value"></param>
+        public static void UpdateColumn(Board board, int row, int column, int value)
+        {
+            for (int index = 0; index < board.Dimensions; index++)
+            {
+                Cell cell = board.GetCell(index, column);
+                if (cell.FinalValue == 0 && cell.PossibleValues.Contains(value))
+                    cell.PossibleValues.Remove(value);
+
+            }
+        }
+        /// <summary>
+        ///  Iterate over the box and remove the number given from
+        ///  the possible values in the cell
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="row"></param>
+        /// <param name="column"></param>
+        /// <param name="value"></param>
+        public static void UpdateBox(Board board, int row, int column, int value)
+        {
+            int sqrtOfDimension = (int)Math.Sqrt(board.Dimensions);
+
+            //Calculate the first cell of the box in which the cell is in
+            int boxRow = (row / sqrtOfDimension) * sqrtOfDimension;
+            int boxColumn = (column / sqrtOfDimension) * sqrtOfDimension;
+
+            for (int index = 0; index < board.Dimensions; index++)
+            {
+                Cell cell = board.GetCell(boxRow + index / sqrtOfDimension, boxColumn + index % sqrtOfDimension);
+                if (cell.FinalValue == 0 && cell.PossibleValues.Contains(value))
+                    cell.PossibleValues.Remove(value);
+
+            }
+        }
+    }
+}
