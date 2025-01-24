c7beb7046a8f55988ef87ba6437ff5b42dbc4d59
ori asulin <oriasulin0909@gmail.com>
2025-01-24 18:15:30 +0200

    feat: add cell class

--- BoardManagement/Board.cs
+++ BoardManagement/Board.cs
@@ -1,41 +1,43 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 
 
-namespace Sudoku.Board  
+namespace Sudoku.BoardManagement 
 {
     /// <summary>
     /// The sudoku board will be represented as a matrix
-    /// of hash sets. Each set will include the numbers
+    /// of hash sets. Each set will include the values
     /// which can fit in the cell.
     /// The board size may vary according to the _dimensions field.
+    /// A classic sudoku board is 9X9.
     /// </summary>
     internal class Board
     {
         
-        private readonly int _dimensions;
-        private readonly ISet<int>[,] _board;
+        public int Dimensions { get; }
+        private readonly Cell[,] _board;
 
         public Board(int dimensions)
         {
-            _dimensions= dimensions;
-            _board = new HashSet<int>[dimensions, dimensions];
+            Dimensions= dimensions;
+            _board = new Cell[dimensions, dimensions];
 
             //initiating each cell
             for (int row = 0; row < dimensions; row++)
             {
                 for (int column = 0; column < dimensions; column++)
                 {
-                    _board[row,column] = new HashSet<int>();
+                    _board[row, column] = new Cell(Dimensions);
                 }
             }
 
         }
 
         //Returns a the set from a specified cell
-        public ISet<int> GetCell(int row, int col) => _board[row, col];
+        public Cell GetCell(int row, int col) => _board[row, col];
 
         //Returns if a cell is in the board
-        public bool InBoard(int row, int col) => row >= 0 && row < _dimensions && col >= 0 && col < _dimensions;
+        public bool InBoard(int row, int col) => row >= 0 && row < Dimensions && col >= 0 && col < Dimensions;
     }
 }
---
+++ BoardManagement/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.BoardManagement
+{
+    internal class Cell
+    {
+        public ISet<int> _possibleValues { get; }
+        public int FinalValue { get; set; } 
+
+        public Cell(int dimensions)
+        {
+            _possibleValues = new HashSet<int>(Enumerable.Range(1, dimensions));
+        }
+    }
+}
