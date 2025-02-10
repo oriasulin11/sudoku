f71d1c9102fb68235858e23a6ddb1b145790ea91
ori asulin <oriasulin0909@gmail.com>
2025-02-10 16:35:00 +0200

    feat: add tests xunit

---
+++ SudokuTester/SudokuTests.cs
@@ -0,0 +1,16 @@
+using Sudoku;
+
+namespace SudokuTester
+{
+    public class SudokuTests
+    {
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
+        [InlineData(9)]
+        [InlineData(16)]
+        [InlineData(25)]
+        public void 
+    }
+}
